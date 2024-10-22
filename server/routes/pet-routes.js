ed806ef490269598fe99e5c0488448e434145eab
Isabela Palmerio <belpalmerio@hotmail.com>
2024-10-22 15:02:54 -0300

    Add multer to handle file uploads

--- server/routes/pet-routes.js
+++ server/routes/pet-routes.js
@@ -4,13 +4,18 @@ const knex = initKnex(configuration);
 import * as petController from "../controllers/pet-controller.js";
 import express from "express";
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router
   .route("/:id")
   .get(petController.findOne)
-  .put(petController.edit)
+  .put(upload.single("petImage"), petController.edit)
   .delete(petController.remove);
 
-router.route("/").get(petController.findAll).post(petController.create);
+router
+  .route("/")
+  .get(petController.findAll)
+  .post(upload.single("petImage"), petController.create);
 
 export default router;
--- server/routes/record-routes.js
+++ server/routes/record-routes.js
@@ -3,14 +3,20 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 import * as recordController from "../controllers/record-controller.js";
 import express from "express";
+import multer from "multer";
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router
   .route("/:id")
   .get(recordController.findOne)
-  .put(recordController.edit)
+  .put(upload.single("recordFile"), recordController.edit)
   .delete(recordController.remove);
 
-router.route("/").get(recordController.findAll).post(recordController.create);
+router
+  .route("/")
+  .get(recordController.findAll)
+  .post(upload.single("recordFile"), recordController.create);
 
 export default router;
